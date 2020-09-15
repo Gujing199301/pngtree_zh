5eb344f85f0641d894731923ff93759a0acc6a65
Gujing199301 <46989399+Gujing199301@users.noreply.github.com>
2020-09-15 16:55:32 +0800

    Update test.py

--- case/test.py
+++ case/test.py
@@ -4,85 +4,85 @@ from time import sleep
 from selenium import webdriver
 # noinspection PyUnresolvedReferences
 import pytest
+import unittest
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-# 创建浏览器对象
-driver = webdriver.Chrome()
+class TestZhLogin(unittest.TestCase):
 
-# 英语测试权限
+    # 初始化操作
+    def setup_class(self):
+        # 创建浏览器对象
+        self.driver = webdriver.Chrome()
 
-url = "https://zh.pngtree.com/"
-# 谷歌浏览器打开测试权限
-driver.get(url)
+        # 英语测试权限
+        url = "https://zh.pngtree.com/"
 
-# 浏览器窗口最大化
-driver.maximize_window()
-sleep(3)
+        # 谷歌浏览器打开测试权限
+        self.driver.get(url)
 
-# 点击导航登录按钮
-driver.find_element_by_xpath('//*[@id="v2-head"]/div/div[1]/div[4]/a[1]').click()
-sleep(3)
+        # 浏览器窗口最大化
+        self.driver.maximize_window()
 
-# 点击facebook登录
-driver.find_element_by_xpath('//*[@id="base-public-login"]/div[2]/div/div/div/div[1]/div/a[1]').click()
-sleep(3)
+        # 点击搜索词
+        self.driver.find_element_by_xpath('//*[@id="wrapper"]/div[6]/div[2]/div/div[2]/div[1]/div[2]/a[2]').click()
+        sleep(5)
 
-# 新打开一个窗口（弹窗）,切换至第2个窗口
-f = driver.window_handles
-driver.switch_to.window(f[1])
+        # 点击登录
+        self.driver.find_element_by_xpath('//*[@id="v2-head"]/div/div[1]/div[4]/a[1]').click()
 
-# 填写fecebool账号
-driver.find_element_by_xpath('//*[@id="email"]').send_keys('2505312014@qq.com')
-# 输入facebook密码
-driver.find_element_by_xpath('//*[@id="pass"]').send_keys('pngtree2019')
+    # line登录
+    def test_zh_line(self):
+        # 点击line登录
+        self.driver.find_element_by_xpath('//*[@id="base-public-login"]/div[2]/div/div/div/div[1]/div/a[2]/i').click()
+        sleep(1)
 
-# 点击登录按钮
-driver.find_element_by_xpath('//*[@id="loginbutton"]').click()
+        # 新打开一个窗口（弹窗）,切换至第2个窗口
+        f = self.driver.window_handles
+        self.driver.switch_to.window(f[1])
 
-sleep(2)
+        # 填写line账号
+        self.driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div[2]/div/form/fieldset/div[1]/input').send_keys('1165509917@qq.com')
 
-# facebook登录窗口自行关闭，切换至原来窗口（登录首页）
-f = driver.window_handles
+        # 输入line密码
+        self.driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div[2]/div/form/fieldset/div[2]/input').send_keys('gujing199301')
 
-driver.switch_to.window(f[0])
+        # 点击登录按钮
+        self.driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div[2]/div/form/fieldset/div[3]/button').click()
 
-print('繁体facebook登录成功')
+        sleep(2)
 
-sleep(3)
+        # line登录窗口自行关闭，切换至原来窗口（登录首页）
+        f = self.driver.window_handles
 
-# 多刷新几次页面，繁体有第4次PV弹优惠券
-driver.refresh()
-sleep(1)
+        self.driver.switch_to.window(f[0])
 
-driver.refresh()
-sleep(1)
+        print('繁体line登录成功')
 
-driver.refresh()
-sleep(1)
+        # 刷新页面，可能有优惠券遮罩
+        self.driver.refresh()
 
-driver.refresh()
-sleep(1)
+        sleep(3)
 
-# 定位用户头像（邮箱登录头像和其他登录方式xpath略有不同）
-user_img = driver.find_element_by_xpath(
-    '//*[@id="v2-head"]/div/div[1]/div[5]/a[1]/div/img')
+        # 定位用户头像（邮箱登录头像和其他登录方式xpath略有不同）
+        user_img = self.driver.find_element_by_xpath(
+            '//*[@id="v2-head"]/div/div[1]/div[5]/a[1]/div/img')
 
-# 创建鼠标对象
-action = ActionChains(driver)
+        # 创建鼠标对象
+        action = ActionChains(self.driver)
 
-action.move_to_element(user_img).perform()
-sleep(3)
+        action.move_to_element(user_img).perform()
+        sleep(3)
 
-# 点击退出账号
-driver.find_element_by_xpath(
-    '//*[@id="v2-head"]/div/div[1]/div[5]/div/div[4]/a[5]/i').click()
-
+        # 点击退出账号
+        self.driver.find_element_by_xpath(
+            '//*[@id="v2-head"]/div/div[1]/div[5]/div/div[4]/a[5]/i').click()
 
 
-sleep(2)
-driver.quit()
 
+    def teardown_class(self):
 
+        sleep(2)
+        self.driver.quit()
 
 
